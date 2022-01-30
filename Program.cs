3c22e390cd7ae1fcaf2d6fe542d29a16e0ac3419
AVoit <AVoit@myemail.com>
2022-01-30 14:56:15 +0500

    create code

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string [] startArray = {"привет", "текст", "three", "три", "на", "о", "if", "for"};
+string [] finalArray = new string [0];
+
+for (int i = 0; i < startArray.Length; i++)
+{
+    int j = 0;
+    if (startArray[i].Length < 4)
+    { 
+        Array.Resize(ref finalArray, j + 1);
+        finalArray[j] = startArray[i];
+        Console.Write(finalArray[j] + " ");
+    }
+}
