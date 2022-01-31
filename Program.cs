caa2e2e297325f2de73902fb79d944062961eb05
AVoit <AVoit@myemail.com>
2022-01-31 09:50:52 +0500

    modified code

--- Program.cs
+++ Program.cs
@@ -8,6 +8,6 @@ for (int i = 0; i < startArray.Length; i++)
     { 
         Array.Resize(ref finalArray, j + 1);
         finalArray[j] = startArray[i];
-        Console.Write(finalArray[j] + " ");
     }
+    Console.Write(String.Join(" ", finalArray));
 }
